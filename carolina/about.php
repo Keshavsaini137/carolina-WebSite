f23550fd009d476273f4b88fe4646cb2513d49a6
Keshav <Keshavsaini137@gmail.com>
2018-07-31 18:58:00 +0530

    Added carolina

---
+++ carolina/about.php
@@ -0,0 +1,34 @@
+<?php
+$title = 'About Us';
+include 'templates/header.php';
+include 'templates/navigation.php';
+?>
+
+<div class="container pt-4">
+     <div class="row no-gutters">
+          <div class="col-12 hero">
+               <img src="img/about_us.jpg" class="img-fluid">
+               <h2 class="text-uppercase">About Us</h2>
+          </div>
+     </div>
+</div>
+
+<div class="container py-4">
+     <div class="row newhero">
+          <main class="col-lg-8 main-content">
+               <p>render() Asynchronously generates the text using the default generator in the lorem-ipsum module, and renders it within the lorem-ipsum element. Defaults not currently specified above under attributes are as described in the documentation for that module. Multiple calls to .render() may be made before the text is actually rendered; the attributes.</p> <p>Lorem of the element at the time of the most recent call determine the final rendering. Uncomment the lines indicated in the screenshot below to test this for yourself: dule. Multiple calls to .render() may be made before the te</p>
+          </main>
+
+          <aside class="col-lg-4">
+               <div class="sidebar">
+                    sidebar
+               </div>
+          </aside>
+
+     </div>
+
+</div>
+
+<?php
+include 'templates/footer.php';
+?>
---
+++ carolina/templates/footer.php
@@ -0,0 +1,44 @@
+<footer class="site-footer pt-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <h3 class="text-uppercase text-center pb-4">About Us</h3>
+        <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae voluptatem nisi possimus autem commodi ex doloribus consectetur!</p>
+      </div>
+      <div class="col-md-4 pb-4 pb-md-0">
+        <h3 class="text-uppercase text-center pb-4">Open Hours</h3>
+        <p class="text-center mb-0">Mon-Fri: 9 AM - 7 PM</p>
+        <p class="text-center mb-0">Saturday: 10 AM - 2 PM</p>
+        <p class="text-center mb-0">Sunday: Closed</p>
+      </div>
+      <div class="col-md-4">
+        <h3 class="text-uppercase text-center pb-4">Contact</h3>
+        <p class="mb-0">Lorem ipsum dolor sit amet.</p>
+        <p class="mb-0">Lorem ipsum dolor.</p>
+
+        <div class="social-nav">
+          <nav class="socials text-left pt-3">
+            <ul class="p-0">
+              <li><a href="http://facebook.com"><span class="sr-only">Facebook</span></a></li>
+              <li><a href="http://twitter.com"><span class="sr-only">Twitter</span></a></li>
+              <li><a href="http://instagram.com"><span class="sr-only">Instagram</span></a></li>
+              <li><a href="http://pinterest.com"><span class="sr-only">Pinterest</span></a></li>
+              <li><a href="http://youtube.com"><span class="sr-only">Youtube</span></a></li>
+            </ul>
+          </nav>
+        </div>
+
+      </div>
+      <div class="w-100"></div>
+      <hr class="w-100">
+      <p class="copyright text-center w-100">Carolina Spa & Salon 2018</p>
+    </div><!--.row-->
+  </div><!--.container-->
+</footer>
+
+<script src="js/jquery.slim.js"></script>
+<script src="js/popper.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/scripts.js"></script>
+</body>
+</html>
